e74f37e998d0b308daabf54af7d849482696bc05
Obieda Ananbeh <oananbeh@outlook.com>
2025-04-18 02:12:02 -0400

    fix issue

---
+++ backEnd/src/test/java/dev/obie/VulnFixAIApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.obie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VulnFixAIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
